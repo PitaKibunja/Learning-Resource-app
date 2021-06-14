1cc338e6ea5b67ab34d6bd38797bfb9acea592a9
dapintechnologies <dapintech@gmail.com>
2021-06-14 06:12:12 +0300

    First Section

---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <div>
+            <h3>{{ title }}</h3>
+            <button>Delete</button>
+        </div>
+        <p>{{ description }}</p>
+        <nav>
+            <a :href="link">View Resource</a>
+        </nav>
+    </li>
+</template>
+<script>
+export default {
+    props:['title','description','link']
+}
+</script>
